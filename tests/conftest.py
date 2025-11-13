d185fe643be70e31d777aa0aa7e18eca7c8a26ca
kris-zvereva <krs.zvrv@gmail.com>
2025-11-13 16:21:54 +0100

    updated conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,5 +8,12 @@ def set_browser():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
+    # для Jenkins
+    options = browser.config.driver_options
+    options.add_argument('--headless=new')  # новый headless режим
+    options.add_argument('--no-sandbox')  # нужно для Jenkins
+    options.add_argument('--disable-dev-shm-usage')  # избегает проблем с памятью
+    options.add_argument('--disable-gpu')  # отключает GPU
+
     yield
     browser.quit()
